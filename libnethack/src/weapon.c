05dd4b796fa5a2bbe6d6cd720719ed333b92ad2b
Jonadab the Unsightly One <jonadab@columbus.rr.com>
2015-06-27 08:11:06 -0400

    [FOURK] Implement race-based (and inventory-based) skill cap adjustments.
     * Any weapon you get in starting inventory will have a skill cap
       no less than basic.  (Your starting skill was already being
       raised to basic.  This prevents you from being above your max.)
    
     * Dwarves can now always get to at least skilled in pick-axe,
       and can get to expert if a human of the same role would be
       able to get to skilled.
    
     * Gnomes can always get to skilled in crossbow and aklys and can
       go two skill ranks higher than a human of the same role, up to
       a maximum of expert (crossbow) or master (club).
    
     * Orcs can get at least skilled in scimitar and two ranks higher
       than a human of the same role up to a maximum of master.
    
     * Elves can always get at least basic skill in bows, regardless
       of their role, and can get to skilled if a human of the same
       role could get to basic. TODO: consider raising max to Master
    
    ---=== ALSO ===---
    
    Fix bugs in the race-based skill cap augmentation.
    
    There were two bugs here, one in the player's favor and one against.
    
    The count wasn't being respected, so the cap was always being bumped
    to the maximum.
    
    However, the game was ignoring the maximum if it had previously
    been zero.  Raising the current skill level to unskilled if it
    was previously restricted fixes this.

--- libnethack/src/weapon.c
+++ libnethack/src/weapon.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-06 */
+/* Last modified by Yer mivvaggah, 2017-11-13 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1482,8 +1482,10 @@ skill_init(const struct def_skill *class_skill)
         if (obj->otyp == TOUCHSTONE || obj->otyp == LUCKSTONE)
             continue;
         skill = weapon_type(obj);
-        if (skill != P_NONE)
-            P_SKILL(skill) = P_BASIC;
+        if (skill != P_NONE) {
+            P_MAX_SKILL(skill) = P_BASIC;
+            P_SKILL(skill)     = P_BASIC;
+        }
     }
 
     /* set skills for magic */
