dc7b92b7f1c090f18e647ed4d504e41f1c0d3565
yrmvgh <yrmvgh@github.com>
2017-11-08 07:50:53 -0700

    make spfx_displacement actually exist

--- libnethack/include/artifact.h
+++ libnethack/include/artifact.h
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-02 */
+/* Last modified by Yer mivvaggah, 2017-11-15 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -39,6 +39,7 @@
 # define SPFX_DBONUS 0x1F00000L /* attack bonus mask */
 # define SPFX_XRAY   0x2000000L /* gives X-RAY vision to player */
 # define SPFX_REFLECT 0x4000000L/* Reflection */
+# define SPFX_DISPL  0x8000000L	/* DIsplacement */
 
 
 struct artifact {
