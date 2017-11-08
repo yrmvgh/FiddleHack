76df764cf87a7f49b6e1ebebd040b6f98d9976ce
yrmvgh <yrmvgh@github.com>
2017-11-08 04:58:31 -0700

    remove the max carry cap

--- libnethack/src/hack.c
+++ libnethack/src/hack.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-06 */
+/* Last modified by Yer mivvaggah, 2017-11-15 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -3228,8 +3228,6 @@ weight_cap(void)
         ||(u.usteed && strongmonst(u.usteed->data)))
         carrcap = MAX_CARR_CAP;
     else {
-        if (carrcap > MAX_CARR_CAP)
-            carrcap = MAX_CARR_CAP;
         if (!Flying) {
             if (leg_hurtl(u.usteed ? u.usteed : &youmonst))
                 carrcap -= 100;
