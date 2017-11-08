b6f9fa4a84ecf5fe57714219be68929395786da9
yrmvgh <yrmvgh@github.com>
2017-11-08 08:19:29 -0700

    make tsurugi chaotic, which matches lore

--- libnethack/include/artilist.h
+++ libnethack/include/artilist.h
@@ -242,7 +242,7 @@ static const struct artifact const_artilist[] = {
     A("The Tsurugi of Muramasa", TSURUGI,
       (SPFX_NOGEN | SPFX_RESTR | SPFX_INTEL | SPFX_BEHEAD | SPFX_LUCK), 0, 0,
       PHYS(0, 0), NO_DFNS, NO_CARY,
-      SELF_UNCURSE, A_LAWFUL, PM_SAMURAI, NON_PM, 4500L),
+      SELF_UNCURSE, A_CHAOTIC, PM_SAMURAI, NON_PM, 4500L),
 
     A("The Platinum Yendorian Express Card", CREDIT_CARD,
       (SPFX_NOGEN | SPFX_RESTR | SPFX_INTEL | SPFX_DEFN),
