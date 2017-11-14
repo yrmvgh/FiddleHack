0d3c8b427ced9757244f30e1cfe26d733d5e0b70
Yer mivvaggah <yrmvgh@users.noreply.github.com>
2017-11-13 18:45:27 -0700

    Rejigger HP: dwarves great early, humans scale
    Race    pre-cutoff      post-cutoff     starting
    Human   d1              2               2
    Elf     d1              1               1
    Dwarf   d5              0               4
    Gnome   d1              0               1
    Orc     d1              0               2
    
    Elves retain 1 hp "post-cutoff" only as a compensation for their
    lower Constitution cap, which I assume is already the reason for
    it to have been this way. TODO: reconsider this later

--- libnethack/src/role.c
+++ libnethack/src/role.c
@@ -375,7 +375,7 @@ const struct Race races[] = {
      {3, 3, 3, 3, 3, 3},
      {18, 18, 18, 18, 18, 20},
      /* Init Lower Higher */
-     {2, 0, 0, 2, 1, 0},        /* Hit points */
+     {2, 0, 0, 1, 2, 0},        /* Hit points */
      {1, 0, 2, 0, 2, 0} /* Energy */
      },
     {"elf", "elven", "elvenkind", "Elf",
@@ -399,7 +399,7 @@ const struct Race races[] = {
      {3, 3, 3, 3, 3, 3},
      {STR18(100), 16, 16, 16, 20, 16},
      /* Init Lower Higher */
-     {4, 0, 0, 3, 2, 0},        /* Hit points */
+     {4, 0, 0, 5, 0, 0},        /* Hit points */
      {0, 0, 0, 0, 0, 0} /* Energy */
      },
     {"gnome", "gnomish", "gnomehood", "Gno",
@@ -423,7 +423,7 @@ const struct Race races[] = {
      {3, 3, 3, 3, 3, 3},
      {STR18(100), 16, 16, 18, 18, 16},
      /* Init Lower Higher */
-     {1, 0, 0, 1, 0, 0},        /* Hit points */
+     {2, 0, 0, 1, 0, 0},        /* Hit points */
      {1, 0, 1, 0, 1, 0} /* Energy */
      },
 /* Array terminator */
