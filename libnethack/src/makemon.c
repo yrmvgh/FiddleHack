d5deebe1655e048d63cd6d8ef7c01d5a7a7b5639
Yer mivvaggah <yrmvgh@users.noreply.github.com>
2017-11-26 10:24:30 -0700

    non-basic liches start with unholy water
    insert joke about basic liches

--- libnethack/src/makemon.c
+++ libnethack/src/makemon.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-02 */
+/* Last modified by Yer mivvaggah, 2017-11-29 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -648,6 +648,10 @@ m_initinv(struct monst *mtmp, enum rng rng)
         }
         break;
     case S_LICH:
+        if (ptr != &mons[PM_LICH])
+            otmp = mksobj(lev, POT_WATER, FALSE, FALSE, rng);
+            curse(otmp);
+            mpickobj(mtmp, otmp, NULL);
         if (ptr == &mons[PM_MASTER_LICH] && !rn2_on_rng(13, rng))
             mongets(mtmp, (rn2_on_rng(7, rng) ? ATHAME : WAN_NOTHING), rng);
         else if (ptr == &mons[PM_ARCH_LICH] && !rn2_on_rng(3, rng)) {
